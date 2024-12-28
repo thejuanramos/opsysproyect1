cfcbb5ae9f09f02c748e56e4eb0ee1dcdc4ca6bc
MarcosFDZGLEZ <1marcosgon@gmail.com>
2024-12-28 11:27:44 +0100

    Update simul_ext_esqueleto.c
    I add functions LeeSuperBloque, Printbytemaps and Directorio.
    Also added to the main function the different type of commands we need to be write in the simul_ext.
    The funtion LeeSuperBloque use a pointer to the super block structure in headers.h and print the content of it.
    The function Bytemaps iterates the first 25 inodes and blocks, it use a pointer to the bytemaps structure on headers.h and print the content of it.
    The function Directorio uses the directorio and inode structure, for the inode we assign this structure ro the inode associated with the directory entry, then we point to the structures to get the information and print it.

--- Proyect II files/simul_ext_esqueleto.c
+++ Proyect II files/simul_ext_esqueleto.c
@@ -60,84 +60,114 @@ int main()
      
      // Buce de tratamiento de comandos
      for (;;){
-		 do {
-		 printf (">> ");
-		 fflush(stdin);
-		 fgets(comando, LONGITUD_COMANDO, stdin);
-		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
-	     if (strcmp(orden,"dir")==0) {
-            Directorio(&directorio,&ext_blq_inodos);
-            continue;
-         }else if (strcmp(orden,"info")==0){
-            LeeSuperBloque(&psup);
-            continue;
+	do {
+		printf (">> ");
+		fflush(stdin);
+		fgets(comando, LONGITUD_COMANDO, stdin);
+	} while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
+		if (strcmp(orden,"dir")==0) {
+            		Directorio(directorio,&ext_blq_inodos);
+            		continue;
+         	}else if (strcmp(orden,"info")==0){
+            		LeeSuperBloque(&psup);
+           		continue;
+		}else if (strcmp(orden, "bytemaps")==0){
+            		Printbytemaps(&ext_bytemaps);
+            		continue;
+         	}else if (strcmp(orden, "rename")==0){
+            		Renombrar(directorio, &ext_blq_inodos, argumento1, argumento2);
+            		continue;
+         	}else if (strcmp(orden, "print")==0){
+            		Imprimir(directorio, &ext_blq_inodos, memdatos, argumento1);
+            		continue;
+         	}else if (strcmp(orden, "remove")==0){
+            		Borrar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, argumento1, fent);
+           		continue;
+         	}else if (strcmp(orden, "copy")==0){
+            		Copiar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, memdatos, argumento1, argumento2,  fent);
+            		continue;
+         	}
          }
          ...
          // Escritura de metadatos en comandos rename, remove, copy     
-         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
+         Grabarinodosydirectorio(directorio,&ext_blq_inodos,fent);
          GrabarByteMaps(&ext_bytemaps,fent);
          GrabarSuperBloque(&ext_superblock,fent);
          if (grabardatos)
-           GrabarDatos(&memdatos,fent);
+           GrabarDatos(memdatos,fent);
          grabardatos = 0;
          //Si el comando es salir se habrán escrito todos los metadatos
          //faltan los datos y cerrar
          if (strcmp(orden,"salir")==0){
-            GrabarDatos(&memdatos,fent);
+            GrabarDatos(memdatos,fent);
             fclose(fent);
             return 0;
          }
      }
 }
 
+// Function to read and display the SuperBlock information
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup){
 
+   // Print the total number of inodes
    printf("Inodes: %u\n", psup->s_inodes_count);
+   // Print the total number of blocks
    printf("Blocks: %u\n", psup->s_blocks_count);
+   // Print the number of free blocks
    printf("Free Blocks: %u\n", psup->s_free_blocks_count);
-   printf("Free Inodes: %u\n", psusp->s_free_inodes_count);
+   // Print the number of free inodes
+   printf("Free Inodes: %u\n", psup->s_free_inodes_count);
+   // Print the index of the first data block
    printf("First Data Block: %u\n", psup->s_first_data_block);
+   // Print the size of a block in bytes
    printf("Block Size: %u\n", psup->s_block_size);
 
 }
 
+// Function to display the inode and block bytemaps
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps){
 
    printf("Inode Bytemap: ");
+   
+   // Iterate over the inode bytemap and print each entry
    for (int i = 0; i < MAX_INODOS; i++){
-      printf("%d ", ext_bytemaps->bmap_inodos);
+      printf("%d ", ext_bytemaps->bmap_inodos[i]);
    }
 
    printf("\n Block Bytemap: ");
+   
+   // Iterate over the block bytemap and print each entry
    for (int i = 0; i < 25; i++){
-      printf("%d ", ext_bytemaps->bmap_bloques);
+      printf("%d ", ext_bytemaps->bmap_bloques[i]);
    }
 
+   printf("\n");
 }
 
+
+// Function to display the contents of the directory
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos) {
 
-    printf("Name\tSize\tInode\tBlocks\n");
+   printf("Directory Contents:\n");
+   printf("Name\t\t\tSize\tInode\tBlocks\n");
 
-    for (int i = 0; i < MAX_FICHEROS; i++) {
+      // Iterate over all directory entries
+      for (int i = 0; i < MAX_FICHEROS; i++) {
 
         if (directorio[i].dir_inodo != NULL_INODO) {
-
+            // Fetch the inode associated with the directory entry
             EXT_SIMPLE_INODE *inode = &inodos->blq_inodos[directorio[i].dir_inodo];
-            printf("%s\t%d\t%d\t", directorio[i].dir_nfich, inode->size_fichero, directorio[i].dir_inodo);
+            // Print file details: name, size, and inode
+            printf("%-16s\t%d\t%d\t", directorio[i].dir_nfich, inode->size_fichero, directorio[i].dir_inodo);
 
+            // Print the block indices associated with the file
             for (int j = 0; j < MAX_NUMS_BLOQUE_INODO; j++) {
-
                 if (inode->i_nbloque[j] != NULL_BLOQUE) {
                     printf("%d ", inode->i_nbloque[j]);
                 }
-
             }
-
             printf("\n");
-
-        }
-
-    }
-
+         }
+      }
 }
+
